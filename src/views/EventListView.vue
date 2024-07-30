d1bd918548ba3b3987f188c4b3df26123e786da6
MRSMIRROR <hunsawut_h@cmu.ac.th>
2024-07-30 11:21:59 +0700

    question 2.5

--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -32,8 +32,10 @@
   <!-- new element -->
   <div class="events">
     <EventCard v-for="event in events" :key="event.id" :event="event"/>
-    <EventDetails v-for="event in events" :key="event.id" :event="event"/>
+    <StudentList v-for="event in events" :key="event.id" :event="event"/>
   </div>
+  <RouterLink :to="{ name: 'event-list-view', query: { page: page - 1 } }" rel="prev" v-if="page != 1">Prev Page</RouterLink>
+  <RouterLink :to="{ name: 'event-list-view', query: { page: page + 1 } }" rel="next">Next Page</RouterLink>
 </template>
 
 <style scoped>
