c3e5a2ba0d0f5805d28dfad2f10c587fd0496b4c
MRSMIRROR <hunsawut_h@cmu.ac.th>
2024-08-04 23:03:42 +0700

    question 4.1

--- src/views/event/RegisterView.vue
+++ src/views/event/RegisterView.vue
@@ -8,8 +8,12 @@ const props = defineProps<{
 }>()
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const { event } = toRefs (props)
+const register = () => {
+    // if the registration API call successful
+    // Push back to the event details view
+}
 </script>
-
 <template>
   <p>Register event here</p>
+  <button @click="register">Register</button>
 </template>
