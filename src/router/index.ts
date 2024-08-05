2c865199ce888bf027320c676cf44ef88ecce750
MRSMIRROR <hunsawut_h@cmu.ac.th>
2024-08-05 13:26:41 +0700

    question 5.1

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,6 +6,7 @@ import EventRegisterView from '@/views/event/RegisterView.vue'
 import EventEditView from '@/views/event/EditView.vue'
 import EventLayoutView from '@/views/event/LayoutView.vue'
 import StudentView from '@/views/StudentView.vue'
+import NotFoundView from '@/views/NotFoundView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -54,6 +55,17 @@ const router = createRouter({
       path: '/Student',
       name: 'Student',
       component: StudentView
+    },
+    {
+      path: '/404/:resource',
+      name: '404/resource-view',
+      component: NotFoundView,
+      props: true
+    },
+    {
+      path: '/:catchAll(.*)',
+      name: 'not-found',
+      component: NotFoundView
     }
   ]
 })
